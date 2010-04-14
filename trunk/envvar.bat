048ddd047edcc30e90893aa253c6bb6397ce8352
ru.elric <ru.elric@5714b17d-ea0f-2632-034c-043ac36cd1db>
2010-04-14 16:52:25 +0000

    + envvar: added ability to specify target (user/system). * envvar: now changes to system dir correctly.

--- trunk/envvar.bat
+++ trunk/envvar.bat
@@ -2,15 +2,33 @@
 
 rem Go to system root to be sure that we're running native find, reg, etc...
 pushd .
+%SystemDrive%
 cd %SystemRoot%\system32
-
-set command=%1
-set variable=%2
-set value=%3
 
 if "%1" == "" goto USAGE
 if "%2" == "" goto USAGE
 if "%3" == "" goto USAGE
+if "%4" == "" (
+  set command=%1
+  set target=system
+  set variable=%2
+  set value=%3
+) else (
+  set command=%1
+  set target=%2
+  set variable=%3
+  set value=%4
+)
+
+if "%target%" == "system" (
+  set regpath="HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
+) else (
+  if "%target%" == "user" (
+    set regpath="HKEY_CURRENT_USER\Environment"
+  ) else (
+    goto USAGE
+  )
+)
 
 if "%command%" == "set" goto RUN
 if NOT "%command%" == "app" (
@@ -26,9 +44,9 @@ rem Get old value
 set oldvalue1=
 set oldvalue2=
 rem This works under win7
-for /f "tokens=2,* delims= " %%j in ('reg query "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v %variable%') do (set oldvalue1=%%k)
+for /f "tokens=2,* delims= " %%j in ('reg query %regpath% /v %variable%') do (set oldvalue1=%%k)
 rem This works under win xp, note that the \t character after delims= is *important*
-for /f "tokens=3 delims=	" %%j in ('reg query "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v %variable%') do (set oldvalue2=%%j)
+for /f "tokens=3 delims=	" %%j in ('reg query %regpath% /v %variable%') do (set oldvalue2=%%j)
 
 rem Under win7 oldvalue2 is empty, under win xp oldvalue1 may not be empty
 set oldvalue=
@@ -50,7 +68,7 @@ if "%command%" == "pre" set value=%value%;%oldvalue%
 
 :RUN
 rem Change value.
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /f /v %variable% /t REG_EXPAND_SZ /d "%value%"
+reg add %regpath% /f /v %variable% /t REG_EXPAND_SZ /d "%value%"
 start %~d0%~p0..\utils\EnvChange
 goto END
 
@@ -58,13 +76,17 @@ goto END
 echo env - environment variables modification script
 echo.
 echo USAGE: 
-echo   env command variable value
+echo   env command [target] variable value
 echo.
-echo possible commands:
+echo Possible commands:
 echo   app     Appends the given value to a semicolon-separated list of values 
 echo           stored in a given environment variable.
 echo   pre     Same as app, but prepends.
 echo   set     Sets the value of a given environment variable.
+echo.
+echo Possible targets:
+echo   system  System variables. Default.
+echo   user    User-specific variables (they override system ones).
 goto END
 
 :UNCHANGED
